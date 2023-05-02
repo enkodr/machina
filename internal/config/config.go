877c8465cda5091dc833667c52f3d5fe6f4b27a7
André Sá <enkodr@outlook.com>
2023-05-02 21:44:22 +0100

    Add logic to identify OS for initial MacOS testing

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,25 +3,25 @@ package config
 import (
 	"os"
 	"path/filepath"
+	"runtime"
 
 	"gopkg.in/yaml.v3"
 )
 
 type Config struct {
-	Hypervisor  string      `yaml:"hypervisor"`
-	Connection  string      `yaml:"connection"`
-	Directories Directories `yaml:"directories"`
+	Hypervisor  string      `yaml:"hypervisor,omitempty"`
+	Connection  string      `yaml:"connection,omitempty"`
+	Directories Directories `yaml:"directories,omitempty"`
 }
 
 type Directories struct {
-	Images    string `yaml:"images"`
-	Instances string `yaml:"instances"`
+	Images    string `yaml:"images,omitempty"`
+	Instances string `yaml:"instances,omitempty"`
 }
 
 var (
-	baseDir    = ".local/share/machina"
-	cfgDir     = ".config/machina"
-	hypervisor = "libvirt"
+	baseDir = ".local/share/machina"
+	cfgDir  = ".config/machina"
 )
 
 // LoadConfig loads the configuration from file or creates a new if it's not yet created
@@ -33,8 +33,8 @@ func LoadConfig() *Config {
 
 	// The configuration to be used
 	cfg := &Config{
-		Hypervisor: hypervisor,
-		Connection: "qemu:///system",
+		Hypervisor: getHypervisor(),
+		Connection: getConnection(),
 		Directories: Directories{
 			Images:    filepath.Join(home, baseDir, "images"),
 			Instances: filepath.Join(home, baseDir, "instances"),
@@ -50,4 +50,24 @@ func LoadConfig() *Config {
 	}
 
 	return cfg
+}
+
+func getHypervisor() string {
+	hypervisor := "libvirt"
+
+	if runtime.GOOS == "darwin" {
+		hypervisor = "qemu"
+	}
+
+	return hypervisor
+}
+
+func getConnection() string {
+	conenction := "qemu:///system"
+
+	if runtime.GOOS == "darwin" {
+		conenction = ""
+	}
+
+	return conenction
 }
